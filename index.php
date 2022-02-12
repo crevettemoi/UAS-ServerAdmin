bba48130e30fd2ff409d729dfa653671f945c3ce
crevettemoi <antonius.jeffrey@yahoo.com>
2022-02-13 04:39:56 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+    $conn = mysqli_connect("172.21.0.2", "root", "password", "Trucorp");
+    if(!$conn)
+    {
+        echo "Database error";
+    }
+    $rows = [];
+    $res = $conn->query("SELECT * FROM users");
+    while($row = mysqli_fetch_assoc($res))
+    {
+        $rows[] = $row;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trucorp</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Jabatan</th>
+        </tr>
+        <?php foreach($rows as $row): ?>
+        <tr>
+            <td><?php echo $row["ID"] ?></td>
+            <td><?php echo $row["Nama"] ?></td>
+            <td><?php echo $row["Alamat"] ?></td>
+            <td><?php echo $row["Jabatan"] ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
